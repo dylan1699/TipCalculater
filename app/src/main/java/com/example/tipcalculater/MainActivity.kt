97231bd1ad75664c6dc6714e022b452eeceab162
SaurabhJha <saurabhjha3890@gmail.com>
2022-09-13 13:36:39 +0530

    day2 full functional app

--- app/src/main/java/com/example/tipcalculater/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculater/MainActivity.kt
@@ -1,26 +1,36 @@
 package com.example.tipcalculater
 
 import android.animation.ArgbEvaluator
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.my_custom_dialog.*
 
 
 private const val TAG = "MainActivity"
 private const val INITIAL_TIP_PERCENT = 15
+
 class MainActivity : AppCompatActivity() {
-    private lateinit var etBaseAmount : EditText
-    private lateinit var seekBarTip : SeekBar
-    private lateinit var tvTipPercentLabel : TextView
-    private lateinit var tvTipAmount : TextView
-    private lateinit var tvTotalAmount : TextView
+    private lateinit var etBaseAmount: EditText
+    private lateinit var seekBarTip: SeekBar
+    private lateinit var tvTipPercentLabel: TextView
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
+    //private lateinit var alert_message:TextView
+    private lateinit var etSplit: EditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,11 +41,31 @@ class MainActivity : AppCompatActivity() {
         tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        etSplit = findViewById(R.id.etSplit)
+
+
+        val btn = findViewById<Button>(R.id.btButton)
+        btn.setOnClickListener {
+            val dialogBinding = layoutInflater.inflate(R.layout.my_custom_dialog, null)
+            val myDialog = Dialog(this)
+            myDialog.setContentView(dialogBinding)
+            myDialog.setCancelable(true)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDialog.show()
+            val yesbtn = dialogBinding.findViewById<Button>(R.id.alert_yes)
+            yesbtn.setOnClickListener {
+                myDialog.dismiss()
+
+            }
+
+
+        }
+
 
         seekBarTip.progress = INITIAL_TIP_PERCENT
         tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
         updateTipDescription(INITIAL_TIP_PERCENT)
-        seekBarTip.setOnSeekBarChangeListener(object:SeekBar.OnSeekBarChangeListener{
+        seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 Log.i(TAG, "onProcessChanged $progress")
                 tvTipPercentLabel.text = "$progress%"
@@ -47,58 +77,81 @@ class MainActivity : AppCompatActivity() {
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
 
-        } )
+        })
 
-        etBaseAmount.addTextChangedListener(object: TextWatcher{
+        etBaseAmount.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
-            override fun afterTextChanged(p0: Editable?){
+            override fun afterTextChanged(p0: Editable?) {
 
                 Log.i(TAG, "afterTextChanged $p0")
                 computeTipandTotal()
             }
 
         })
+
+        etSplit.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG, "afterTextChanged $p0")
+                computeTipandTotal()
+            }
+
+        })
+
     }
 
     private fun updateTipDescription(tipPercent: Int) {
-        val tipDescription : String;
-        when (tipPercent){
-            in 0..9->tipDescription="Poor"
-            in 10..14->tipDescription="Acceptable"
-            in 15..19->tipDescription="Good"
-            in 20..24->tipDescription="Great"
-            else ->tipDescription ="Amazing"
+        val tipDescription: String;
+        when (tipPercent) {
+            in 0..9 -> tipDescription = "Poor"
+            in 10..14 -> tipDescription = "Acceptable"
+            in 15..19 -> tipDescription = "Good"
+            in 20..24 -> tipDescription = "Great"
+            else -> tipDescription = "Amazing"
         }
-        tvTipDescription.text= tipDescription
-        val color = ArgbEvaluator().evaluate(tipPercent.toFloat()/seekBarTip.max,
-            ContextCompat.getColor(this,R.color.colorWorstTip),
-            ContextCompat.getColor(this,R.color.colorBestTip)
-        )as Int
+        tvTipDescription.text = tipDescription
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat() / seekBarTip.max,
+            ContextCompat.getColor(this, R.color.colorWorstTip),
+            ContextCompat.getColor(this, R.color.colorBestTip)
+        ) as Int
         tvTipDescription.setTextColor(color)
 
     }
 
+
     private fun computeTipandTotal() {
         //1.Get the value of the base and tip percent
-        if(etBaseAmount.text.isEmpty()){
+        if (etBaseAmount.text.isEmpty()) {
             tvTipAmount.text = ""
-            tvTotalAmount.text=""
+            tvTotalAmount.text = ""
             return
         }
 
         val baseAmount = etBaseAmount.text.toString().toDouble()
         val tipPercent = seekBarTip.progress
+        //val splitNo    = etSplit.text.toString().toDouble()
+
 
         //2. Compute the tip and total
-        val tipAmount = baseAmount*tipPercent/100
-        val totalAmount = baseAmount+tipAmount
+        val tipAmount = baseAmount * tipPercent / 100
+        val totalAmount = baseAmount + tipAmount
+        //val to_pay     = totalAmount/splitNo
+
 
         //3. Update the UI
         tvTipAmount.text = "%.2f".format(tipAmount)
         tvTotalAmount.text = "%.2f".format(totalAmount)
+        //alert_message.text = "%.2f".format(totalAmount)
     }
+
+
+
 }
